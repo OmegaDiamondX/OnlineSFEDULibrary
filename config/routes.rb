df0df54816df7f9190f3464e47c0fa1ffb704e98
Tlotro <50557602+Tlotro@users.noreply.github.com>
2023-11-19 23:34:42 +0000

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #For unknown reasons, all atempts at creating a controller fail, help.
+  root 'SFEDUlib#home'
+  get '\home' to 'SFEDUlib#home'
+  post '\login' to 'SFEDUlib#login'
+  get '\logout' to 'SFEDUlib#logout'
+  get '\:id' to 'SFEDUlib#book'
+  post '\:id' to 'SFEDUlib#take_book'
+  delete '\:id' to 'SFEDUlib#return_book'
+  patch '\:id' to 'SFEDUlib#extend_book'
+  post '\:id\request' to 'SFEDUlib#request_book'
+  get '\:id\:page' to 'SFEDUlib#open_book'
 end
