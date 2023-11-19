b5ae482a7228e21aa18385fd9adb80edd87e52eb
Tlotro <50557602+Tlotro@users.noreply.github.com>
2023-11-19 23:42:43 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  #For unknown reasons, all atempts at creating a controller fail, help.
+  # For unknown reasons, all atempts at creating a controller fail, help.
   root 'SFEDUlib#home'
-  get '\home' to 'SFEDUlib#home'
-  post '\login' to 'SFEDUlib#login'
-  get '\logout' to 'SFEDUlib#logout'
-  get '\:id' to 'SFEDUlib#book'
-  post '\:id' to 'SFEDUlib#take_book'
-  delete '\:id' to 'SFEDUlib#return_book'
-  patch '\:id' to 'SFEDUlib#extend_book'
-  post '\:id\request' to 'SFEDUlib#request_book'
-  get '\:id\:page' to 'SFEDUlib#open_book'
+  get '\home', to: 'SFEDUlib#home'
+  post '\login', to: 'SFEDUlib#login'
+  get '\logout', to: 'SFEDUlib#logout'
+  get '\:id', to: 'SFEDUlib#book'
+  post '\:id', to: 'SFEDUlib#take_book'
+  delete '\:id', to: 'SFEDUlib#return_book'
+  patch '\:id', to: 'SFEDUlib#extend_book'
+  post '\:id\request', to: 'SFEDUlib#request_book'
+  get '\:id\:page', to: 'SFEDUlib#open_book'
 end
